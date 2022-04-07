6335bb8590445fa69e5d0b27d11a0db74c7ce401
S010 <morganjdev@gmail.com>
2022-04-07 17:34:01 +0100

    ITS ALIVE

--- Assessment/main.py
+++ Assessment/main.py
@@ -1,59 +1,114 @@
+import random , os
 from math import exp
-import random
 
-
-class Perceptron:
-  def __init__(self):
-    self.weights  = [None] * 2
-    for i in range(0,len(self.weights)):
-      self.weights[i] = random.randrange(-1,1)
-  
-  def sigmoidFunc(self,net):
-    return 1/(1+exp(-net))
-  
-  def netFunc(self,inputs):
-    net = 0
-    for i in range(0,len(inputs)):
-        temp = inputs[i] * self.weights[i]
-        net = net + temp
-    return net
-
-  def guess(self,inputs):
-    sum = self.netFunc(inputs)
-    output = self.sigmoidFunc(sum)
-    return output
-  
-  def train(self,inputs,target):
-    g = self.guess(inputs)
-    error = target - g
+class Node:
+  def __init__(self,_name,_weights,_inputs,_outputs):
+    self.name = _name
+    self.inputs = _inputs
+    self.weights = _weights
+    self.outputs = _outputs
     
-    #Tuning weights
-    for i in range(0,len(self.weights)):
-      self.weights[i] += error * inputs[i]
-    print(error)
-    return self.weights
-
-
-p = Perceptron()
-
-inputs = [-1,0.5]
-target = 1
-for i in range(10000):
-  print(p.train(inputs,target))
-
-'''
-trainingInputs = [[0,0,1],
-                  [1,1,1],
-                  [1,0,1],
-                  [0,1,1]]
-
-trainingOutputs = [[0,1,1,0]]
-'''
+  def netFunc(self):
+    sum = 0
+    for i in range(0,len(self.inputs)):
+      sum += (self.weights[i] * self.inputs[i])
+    return sum
   
+  def sigmoid(self):
+    return 1/ (1 + exp(-self.netFunc()))
 
+class Network:
+  def __init__(self,_inputs,_hidden,_output,_expected):
+    self.inputs = _inputs
+    self.hidden = _hidden
+    self.output = _output
+    self.expected = _expected
+    print(f"=====\nNew network created\nInputs = {self.inputs}")
+    print(f"Hidden Layer =")
+    for node in self.hidden: print(node.name,"|",node.weights)
+    print(f"Output Layer =")
+    for node in self.output: print(node.name,"|",node.weights)
+  
+  def forwardStep(self):
+    outputs = []
+    for node in self.hidden:
+      outputs.append(node.sigmoid())
+    print(outputs)
+    
 
+def getData(direc):
+  arr = []
+  with open(direc,'r') as fl:
+    for line in fl:
+      line.strip("\n")
+      data = line.split()
+      for i in range(0,len(data)):
+        data[i] = float(data[i])
+      arr.append(data)
+  return arr
 
-#weights = [[0.9, 0.45, 0.36, 0.98,0.92],[0.74,0.13,0.68],[0.8,0.4,0.10],[0.35, 0.97, 0.96],[0.35,0.8],[0.50,0.13],[0.90,0.8]]
-
-#print(n)
  
+def chooseDirectory():
+  listOfDirs = []
+  for(root,dirs,file) in os.walk("./"):
+    for f in file:
+      if '.txt' in f:
+        listOfDirs.append(f)
+  while True:
+    for i in range(0,len(listOfDirs)):
+      print(f"{i+1}:{listOfDirs[i]}")
+    selected = input(">")
+    if selected.isnumeric() == False:
+      print("Invalid input\n")
+    else:
+      if int(selected) > len(listOfDirs) or int(selected) < 0:
+        print("Invalid Input\n")
+      else:
+        return listOfDirs[int(selected)-1]
+      
+
+def populateDataSet(dataSet):
+  newDataSet = []
+  for i in range(len(dataSet)):
+    newDataSet.append([[],[]])
+
+    for j in range(len(dataSet[i])):
+      if j > layers['hidden']-1:
+        newDataSet[i][1].append(dataSet[i][j])
+      else:
+        newDataSet[i][0].append(dataSet[i][j])
+    newDataSet[i][0] = [1.00] + newDataSet[i][0]
+  return newDataSet
+      
+
+def populateNodes(dataSet,layers,weights):
+  nodes = {'hidden':[],'output' : []}
+  for i in nodes:
+    for j in range(0,layers[i]):
+      newNode = Node(f"Node{len(nodes['hidden']+nodes['output'])}",weights[i][j],dataSet,[])
+      nodes[i].append(newNode)
+  return nodes
+
+dataSet = getData(chooseDirectory())
+layers = {'hidden':3,'output': 2}
+epochs = 100
+
+#node [ 4 , 5 , 6 ],7 8
+weights = {'hidden':[[0.9,0.74,0.8,0.35],[0.45,0.13,0.4,0.97],[0.36,0.68,0.1,0.96]],'output':[[0.98,0.35,0.5,0.9],[0.92,0.8,0.13,0.8]]}
+dataSet = populateDataSet(dataSet)
+nodes = populateNodes(dataSet[0][0],layers,weights)
+network = Network(dataSet[0][0],nodes['hidden'],nodes['output'],dataSet[0][1])
+network.forwardStep()
+
+## main stuff
+'''
+for i in range(0,iterations):
+  nodes = populateNodes(dataSet[i][0],layers,weights)
+  network = Network(dataSet[0][0],nodes['hidden'],nodes['output'],dataSet[0][1])
+  for j in range(epochs):
+    ## Do shit
+    # - >> Forward 
+    # - << Backward
+    # - >> work out error Rate
+    # - >> update weights
+'''
