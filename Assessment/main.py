39e23c36f2d14cd175c8f92699b66a412e617638
Jacob Morgan <jake2002.jm@gmail.com>
2022-04-15 11:50:18 +0100

    UPDATED WEIGHTS WOOOO

--- Assessment/main.py
+++ Assessment/main.py
@@ -11,7 +11,6 @@ class Node:
     
   def netFunc(self):
     sum = 0
-
     for i in range(0,len(self.inputs)):
       sum += self.weights[i] * self.inputs[i]
     return sum
@@ -51,27 +50,52 @@ class Network:
       node.outputs = output
 
   def errorFunc(self):
-
-
     #Output errors
     for i in range(len(self.output)):
-      print(self.output[i].name,self.output[i].outputs,self.expected[i])
       self.output[i].error = self.expected[i] - self.output[i].outputs
-      print(self.output[i].error)
-    for i in range(len(self.hidden)):
-      print(self.hidden[i].name,self.hidden[i].outputs)
       
-      #for j in range(len(self.output)):
-  
-      self.hidden[i].error = self.hidden[i].outputs * (1 - self.hidden[i].outputs)* (()+())
-
-  
+    #Hidden Errors
+    for i in range(0,len(self.hidden)):
+      self.hidden[i].error = (self.hidden[i].outputs * (1 - self.hidden[i].outputs) * ((self.output[0].weights[i+1] * self.output[0].error) + (self.output[1].weights[i+1] * self.output[1].error)))
+      
   def updateWeights(self):
-    pass
+    deltaWeights = {'hidden':[[],[],[]],'output':[[],[]]}
+    #Hidden updated weights
+    for i in range(len(self.hidden)):
+      for j in range(len(self.inputs)):
+        newVal = self.learningRate * self.hidden[i].error * self.inputs[j]
+        deltaWeights['hidden'][i].append(newVal)
+        
+    #Output updated weights
+    for i in range(len(self.output)):
+      deltaWeights['output'][i].append(self.learningRate * self.hidden[i].error * 1)
+      for j in range(len(self.hidden)):
+        newVal = self.learningRate * self.output[i].error * self.hidden[j].outputs
+        deltaWeights['output'][i].append(newVal) 
     
-  def backwardProp(self):
-    pass
-    
+    return deltaWeights  
+  
+  def backProp(self):
+    deltaWeights = self.updateWeights()
+    for i in range(len(self.hidden)):
+      node = self.hidden[i]
+      for j in range(len(node.weights)):
+        node.weights[j] = node.weights[j]+ deltaWeights['hidden'][i][j]
+        
+    for i in range(len(self.output)):
+      node = self.output[i]
+      for j in range(0,len(node.weights)):
+        node.weights[j] = node.weights[j]+ deltaWeights['output'][i][j] 
+
+  def outputWeights(self):
+    newWeights = {'hidden':[[],[],[]],'output':[[],[]]}
+    for i in range(len(self.hidden)): 
+      node = self.hidden[i]
+      newWeights['hidden'][i] = node.weights
+    for i in range(len(self.output)): 
+      node = self.output[i]
+      newWeights['output'][i] = node.weights
+    return newWeights
 
 def getData(direc):
   arr = []
@@ -83,7 +107,6 @@ def getData(direc):
         data[i] = float(data[i])
       arr.append(data)
   return arr
-
  
 def chooseDirectory():
   listOfDirs = []
@@ -102,18 +125,13 @@ def chooseDirectory():
         print("Invalid Input\n")
       else:
         return listOfDirs[int(selected)-1]
-      
 
 def populateDataSet(dataSet):
   newDataSet = []
   print("\n\nData Sets\n~~~~~~~~~~~~~~~~~~~~~~~~~")
   print(dataSet)
   for i in range(len(dataSet)):
-    print(i,dataSet[i])
-    print(f"{i}Before {newDataSet}")
     newDataSet.append([[],[]])
-    print(f"{i}After {newDataSet}")
-    print(newDataSet[i])
 
     for j in range(len(dataSet[i])):
       if j > layers['hidden']-1:
@@ -122,8 +140,7 @@ def populateDataSet(dataSet):
         newDataSet[i][0].append(dataSet[i][j])
     newDataSet[i][0] = [1.00] + newDataSet[i][0]
   print("~~~~~~~~~~~~~~~~~~~~~~~~~")
-  return newDataSet
-      
+  return newDataSet   
 
 def populateNodes(dataSet,layers,weights):
   nodes = {'hidden':[],'output' : []}
@@ -142,18 +159,23 @@ def getEpochs():
       print("Invalid Input")
       
   return value
+
+#Testing shit
 dataSet = getData(chooseDirectory())
 layers = {'hidden':3,'output': 2}
 
 #node [ 4 , 5 , 6 ],7 8
 weights = {'hidden':[[0.9,0.74,0.8,0.35],[0.45,0.13,0.4,0.97],[0.36,0.68,0.1,0.96]],'output':[[0.98,0.35,0.5,0.9],[0.92,0.8,0.13,0.8]]}
 dataSet = populateDataSet(dataSet)
-epochs = getEpochs()
+#epochs = getEpochs()
 nodes = populateNodes(dataSet[0][0],layers,weights)
 network = Network(dataSet[0][0],dataSet[0][1],nodes['hidden'],nodes['output'],0.1)
-print(network.inputs,network.expected)
+print(f"Original - {weights}" )
 network.forwardStep()
-#network.errorFunc()
+network.errorFunc()
+network.backProp()
+weights = network.outputWeights()
+print(f"New - {weights}" )
 
 ## main stuff
 '''
